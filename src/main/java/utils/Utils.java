f919303e370a27d76234e433f5a038400c3edef6
Hannes Lowette <hannes.lowette@xplore.be>
2016-07-18 15:57:39 +0200

    Get rid of model.Input file and replace with HashMap

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,17 +1,35 @@
 package utils;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import model.CustomFile;
-import model.Input;
-
+import org.joda.time.DateTime;
 import org.mule.api.MuleEventContext;
 import org.mule.api.lifecycle.Callable;
 
+import model.CustomFile;
+
 public class Utils implements Callable {
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public Object onCall(MuleEventContext eventContext) throws Exception {
-		Map<String, CustomFile> files = ((Input) eventContext.getMessage().getInvocationProperty("input")).getAllFiles();
+		Map<String, CustomFile> files = new HashMap<>();
+		
+		for (HashMap<String, ?> file_entry : (List<HashMap<String, ?>>) ((HashMap<String, ?>) eventContext.getMessage().getInvocationProperty("input")).get("sip_package")) {
+			CustomFile file = new CustomFile();
+
+			file.file_name = file_entry.get("file_name").toString();
+			file.file_path = file_entry.get("file_path").toString();
+			file.file_type = file_entry.get("file_type").toString();
+			file.md5 = file_entry.get("md5").toString();
+			file.timestamp = DateTime.now(); // DateTime.parse(file_entry.get("timestamp").toString());
+			
+			files.put(file.file_type, file);
+		}
+		
 		return files;
 	}
+
 }

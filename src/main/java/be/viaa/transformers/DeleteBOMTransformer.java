fbf9f2c6461b2564153cb05892d6b3faba558bd6
dietervanhoof <dieter.vanhoof@c4j.be>
2019-04-19 13:57:17 +0200

    Return data as a string

--- src/main/java/be/viaa/transformers/DeleteBOMTransformer.java
+++ src/main/java/be/viaa/transformers/DeleteBOMTransformer.java
@@ -1,11 +1,11 @@
 package be.viaa.transformers;
-
 import java.io.InputStream;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.input.BOMInputStream;
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
+
 /**
  * 
  * @author https://stackoverflow.com/users/1864381/countd
@@ -16,8 +16,9 @@ public class DeleteBOMTransformer extends AbstractMessageTransformer {
 	@Override
 	public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
 		try (InputStream in = new BOMInputStream(IOUtils.toInputStream(message.getPayloadAsString()))) {
-			return in;
-		} catch (Exception e) {
+			return IOUtils.toString(in);
+		}
+		catch (Exception e) {
 			throw new RuntimeException("Could not remove BOM marker");
 		}
 	}
